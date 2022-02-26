91a4ca75e276c4c579ba56cdc2fcb2ed5213b925
Sacha Alex Collard Legault <you@example.com>
2022-02-26 16:43:40 -0500

    commit avant mixin

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
     <h1><?php echo get_bloginfo(); ?></h1>
     </a>
-    <h2 class="header__description"> <?php echo get_bloginfo('description'); ?></h2>
+    <h2 class="header__description"> Mon premier thème </h2>
     </section>
     <section class="util">
 
@@ -24,6 +24,7 @@
         <a href="#" class="icone__lien"><svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5"></path></svg></a>
         <a href="#" class="icone__lien"><svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg></a>
         <a href="#" class="icone__lien"><svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"></path></svg></a>
+
 
     </div>
 
