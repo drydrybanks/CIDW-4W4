7fe38fd2b953936ebabc39abfa044d17bccce311
Sacha Legault <alexendre.legault@gmail.com>
2022-03-07 11:02:23 -0500

    03-07-2022

--- footer.php
+++ footer.php
@@ -1,4 +1,55 @@
 <footer class="site__footer">
+
+
+
+
+<!-- Première rangée de colonnes -->
+<div class="site__footer__colonne">
+        <!-- Une list des articles -->
+        <section class="footer__article">
+        <h2>Titre 1</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, assumenda asperiores doloremque deserunt vel itaque temporibus consequatur, sequi nobis quos animi eligendi nisi rerum fugit consectetur accusamus aut esse suscipit!</p>
+        </section>
+        <!-- L'adresse de l'entreprise -->
+        <section class="footer__adresse">
+
+        938373 rue du Fun Noir
+        </section>
+        <!-- Les liens intéressants -->
+        <section class="footer__liens">
+    <?php 
+
+        wp_nav_menu(array("menu"=>"externe"));
+                                ?>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+        </section>
+
+
+
+    </div>
+
+
+<!-- Rangée de lignes -->
+    <div class="site__footer__ligne">
+        <!-- Description du site -->
+        <section class="description"></section>
+        <!-- Le menu du footer -->
+        <section class="footer__nav"></section>
+        <!-- Les droits d'auteurs -->
+        <section class="copyright"></section>
+        <!-- Le nom de l'auteur -->
+        <section class="footer__auteur"></section>
+
+
+
+
+    </div>
+
+
+
     <section>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam rerum quaerat repudiandae enim ullam minima, sit perspiciatis fugiat necessitatibus cum voluptate neque blanditiis facere? Aperiam vero excepturi modi a unde!</p>
     
