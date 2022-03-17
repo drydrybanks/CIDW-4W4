87baebcfcc523aa2f9a96b772002d9abbd606f40
Sacha Alex Collard Legault <e1375494@cmaisonneuve.qc.ca>
2022-03-17 14:48:47 -0400

    ajout incones sociaux

--- functions.php
+++ functions.php
@@ -66,9 +66,23 @@
 
 /* ============ ENREGISTRER LE SIDEBAR ================== */
 
+
     add_action( 'widgets_init', 'my_register_sidebars' );
 function my_register_sidebars() {
     /* Register the 'primary' sidebar. */
+    register_sidebar(
+        array(
+            'id'            => 'entete_1',
+            'name'          => __( 'entete_1' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans l\'entête du site ' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+
+
     register_sidebar(
         array(
             'id'            => 'footer_colonne_1',
---
+++ sidebar-entete_1.php
@@ -0,0 +1,5 @@
+<div id="sidebar-entete_1" class="sidebar">
+
+<?php dynamic_sidebar('entete_1'); ?>
+
+</div>
