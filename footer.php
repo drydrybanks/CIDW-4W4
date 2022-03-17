524091c9138194c72476b97c08fcf8df59543f7a
Sacha Alex Collard Legault <e1375494@cmaisonneuve.qc.ca>
2022-03-16 21:42:08 -0400

    les droits d'auteurs

--- footer.php
+++ footer.php
@@ -65,7 +65,7 @@
 
 
     <section>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam rerum quaerat repudiandae enim ullam minima, sit perspiciatis fugiat necessitatibus cum voluptate neque blanditiis facere? Aperiam vero excepturi modi a unde!</p>
+        <p> Tout droits réservés TIM 2022. </p>
     
 
         <?php
