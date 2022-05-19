2c1b348be9d394326fc085cd2ee57f6eec38a5fa
Sacha Alex Collard Legault <e1375494@cmaisonneuve.qc.ca>
2022-05-19 15:17:46 -0400

    EF-SACL remise epreuve finale

--- gabarit/template-atelier.php
+++ gabarit/template-atelier.php
@@ -16,48 +16,49 @@
        <h1> <?php the_title() ?> </h1>
      <!-- Le titre de la page -->
      <section class="atelier_description">
-    <h6>Description</h6>
-     <?php the_field('description'); ?>
+    <h4>Description</h4>
+    <span class=champs> <?php the_field('description'); ?> </span>
 
      </section>
      <p class="animateur">
-     Cet atelier est donné par :<?php the_field('animateur') ?>
+     Cet atelier est donné par :<span class=champs><?php the_field('animateur') ?></span>
 
 </p>
 <section class="date__lieu">
+    <h4>Horaire et dates</h4>
     
     <p class="date__debut">
-    Cet atelier débute le :<?php the_field('date_debut') ?>
+    Cet atelier débute le :<span class=champs><?php the_field('date_debut') ?> </span>
 
     </p>
 
     <p class="date__fin">
-    Et termine le :<?php the_field('date_fin') ?>
+    Et termine le :<span class=champs><?php the_field('date_fin') ?></span>
 
     </p>
 
     <p class="jours">
-    Cet atelier à lieu les :<?php the_field('jours_semaine') ?>
+    Cet atelier à lieu les :<span class=champs><?php the_field('jours_semaine') ?></span>
 
     </p>
 
     <p class="heure__debut">
-    De :<?php the_field('heure_debut') ?>
+    De :<span class=champs><?php the_field('heure_debut') ?></span>
 
     </p>
 
     <p class="heure__fin">
-    Jusqu'à<?php the_field('heure_fin') ?>
+    Jusqu'à :<span class=champs><?php the_field('heure_fin') ?></span>
 
     </p>
 
     <p class="duree">
-    Pour une durée de : <?php the_field('duree') ?>
+    Pour une durée de :<span class=champs> <?php the_field('duree') ?></span>
 
     </p>
 
     <p class="local">
-    Dans le local :  <?php the_field('local') ?>
+    Dans le local :  <span class=champs><?php the_field('local') ?></span>
 
     </p>
 
